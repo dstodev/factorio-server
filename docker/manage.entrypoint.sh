5e7d328ee682e452a2482707c5d0357a81c637e0
Daniel Stotts <dstodev@gmail.com>
2024-12-28 01:38:29 -0800

    Improve manage entrypoint

--- docker/manage.entrypoint.sh
+++ docker/manage.entrypoint.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 set -euo pipefail
 
+help() {
+	cat <<-EOF
+		Usage: $(basename "$0") [ -u ]
+		  -h, --help    Print this message.
+		  -u, --update  Update the manage project first.
+		  -- [ ... ]    Pass all arguments after -- to the manage program.
+	EOF
+}
+
+canonical=$(getopt --name "$(basename "$0")" \
+	--options hu \
+	--longoptions help,update \
+	-- "$@") || status=$?
+
+if [ "${status-0}" -ne 0 ]; then
+	help
+	exit 1
+fi
+
+eval set -- "$canonical"
+
+while :; do
+	case "$1" in
+	-h | --help)
+		help
+		exit 0
+		;;
+	-u | --update)
+		update_manage=true
+		;;
+	--)
+		shift # --
+		break
+		;;
+	esac
+	shift # option
+done
+
+update_manage=${update_manage-false}
+
 pushd manage >/dev/null
 
-umask 0002
-
 venv_activate() {
-	. .venv/bin/activate
+	# shellcheck source=manage/.venv/bin/activate
+	source .venv/bin/activate
 }
 
 venv_init() {
@@ -16,10 +55,25 @@ venv_init() {
 		pip install --editable .
 }
 
-if [ -d .venv ]; then
+umask 0002
+
+if $update_manage; then
+	clean_files=(
+		.venv
+		build
+		manage.egg-info
+	)
+	for file in "${clean_files[@]}"; do
+		if [ -e "$file" ]; then
+			rm -r "$file"
+		fi
+	done
+fi
+
+if [ -d .venv ] && ! $update_manage; then
 	venv_activate
 else
-	venv_init
+	venv_init # calls venv_activate
 fi
 
 manage "$@"
